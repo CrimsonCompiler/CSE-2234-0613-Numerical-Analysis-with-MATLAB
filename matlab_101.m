52c3e16d7b3981c7ffe32924c6e9f938821f213a
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-13 20:15:18 +0600

    Learned about variables and basic delete print and little operations

---
+++ matlab_101.m
@@ -0,0 +1,51 @@
+# Variables in Matlab ( Though I am doing this on octave )
+
+# Syntax
+
+# [variable_name] = [expression]
+
+a = 2 # automatic result
+
+a = 3; # no - automatic result
+a
+
+b = (1 + 3) / 2
+
+# assigning values
+c = b # c is 2 here
+c = 5 # value changes here
+
+d = b + 1
+
+
+a = a + 1 # updating the variable a with recursively
+
+
+# check what variables used
+who
+
+# more details
+whos
+
+# delete a variable
+clear a b
+
+# delete all
+clear all
+
+
+# Variable naming convention
+
+# 1abc = 10 x
+# a!bcs = 20 x
+# a bc = 5 x
+
+abc = 10
+
+# case sensitive
+Abc = 20
+
+
+# Text variable
+str = 'Tousif'
+
