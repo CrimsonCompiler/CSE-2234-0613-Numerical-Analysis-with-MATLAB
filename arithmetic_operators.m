39196cac74bb1496adf140527da50bde95172795
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-13 20:23:35 +0600

    feat: Add MATLAB script demonstrating arithmetic operators.

---
+++ arithmetic_operators.m
@@ -0,0 +1,16 @@
+# Learning about operators in matlab
+
+a = 2 - 3
+
+a = 2 * 3
+
+a = 2 * 3 + 1
+
+a = 2 * ( 4 * 5 + ( 4/ 2))
+
+pow = 2 ^ 5
+poww = 2 ** 3
+
+3 / 2 # stores int the ans variable
+
+
