ec3ce1c03d20423aaab56582c961d54a2868d9e0
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-14 23:57:40 +0600

    feat: Add examples for vector element access and modification.

--- vectors_beginner.m
+++ vectors_beginner.m
@@ -7,3 +7,16 @@ a = [3 5 -2 0 -1]
 b = [ 1
       2
       3]
+
+# accessing elements
+# In matlab we use 1 based indexing
+a(1) # prints out 3
+a(2) # prints out 5
+
+# Multiple elements access
+a([1 2]) # prints [3, 5]
+
+# Changing value
+a(1) = -2;
+a
+
